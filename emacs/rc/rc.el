fb72f5b416e533dd4e23a6601317bdfb0e75909d
dvraaij <dvraaij@users.noreply.github.com>
2022-11-15 19:37:22 +0100

    Disable GNU devel repository for Emacs.

--- emacs/rc/rc.el
+++ emacs/rc/rc.el
@@ -1,5 +1,5 @@
 (add-to-list 'package-archives '("melpa"     . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("gnu-devel" . "https://elpa.gnu.org/devel/"))
+;; (add-to-list 'package-archives '("gnu-devel" . "https://elpa.gnu.org/devel/"))
 
 ;; (add-to-list 'package-archives
 ;;              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
